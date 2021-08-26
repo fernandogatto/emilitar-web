cc906ca11e49e5b075ee52ad3305b14f8e0f6cf6
Fernando <fernandogatto17@gmail.com>
2021-08-26 01:13:03 -0300

    Adição de página de status do alistamento

--- src/pages/Status/styles.js
+++ src/pages/Status/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 import { withTheme } from '@material-ui/core/styles';
 
-export const ContainerPoints = withTheme(styled.div`
+export const ContainerStatus = withTheme(styled.div`
     width: 100%;
     height: 100vh;
     overflow: hidden;
@@ -16,8 +16,60 @@ export const ContainerPoints = withTheme(styled.div`
     }
 `);
 
-export const ContentPoints = withTheme(styled.div`
+export const ContentStatus = withTheme(styled.div`
     max-width: 1200px;
     width: 100%;
     margin: auto;
+
+    p {
+        color: ${props => props.theme.palette.description.secondary.light};
+        line-height: 21px;
+    }
+
+    .container-grid-area {
+        .item-main, .item-sidebar, .item-footer {
+            background-color: ${props => props.theme.palette.background.primary.main};
+            border-radius: 12px;
+            padding: 32px 16px 16px;
+
+            .container-title {
+                margin-bottom: 16px;
+            }
+        }
+
+        .item-main {
+            .container-description {
+                .item-description + .item-description {
+                    margin-top: 16px;
+                }
+            }
+        }
+
+        .item-sidebar {
+            .container-description {
+                p {
+                    display: flex;
+                    align-items: center;
+                }
+
+                svg {
+                    margin-right: 8px;
+                }
+            }
+        }
+
+        .item-footer {
+            .container-description {
+                .item-description {
+                    & + .item-description {
+                        margin-top: 16px;
+                    }
+
+                    h4 {
+                        font-size: 14px;
+                    }
+                }
+            }
+        }
+    }
 `);
